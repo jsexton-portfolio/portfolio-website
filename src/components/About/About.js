44c3f30ce7faae241397e7b2f8919c3490fbc021
Justin Sexton <jsexton11295@gmail.com>
2020-03-06 22:06:32 -0600

    Removed redundant values

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,21 +15,21 @@ export const About = () => {
 
       <Container className={classes.sectionContainer}>
         <Typography variant='h2'>Early Life</Typography>
-        <Typography paragraph={true}>
+        <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Typography>
       </Container>
 
       <Container className={classes.sectionContainer}>
         <Typography variant='h2'>???</Typography>
-        <Typography paragraph={true}>
+        <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Typography>
       </Container>
 
       <Container className={classes.sectionContainer}>
         <Typography variant='h2'>Future Endeavors</Typography>
-        <Typography paragraph={true}>
+        <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Typography>
       </Container>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -69,7 +69,7 @@ export const Home = (props) => {
 
       <Container className={classes.sectionContainer}>
         <Typography variant='h3'>About Me</Typography>
-        <Typography paragraph={true}>
+        <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Typography>
         <Link to='/about' className={classes.link}>
@@ -85,7 +85,7 @@ export const Home = (props) => {
         <Typography variant='h3'>
           Contact
         </Typography>
-        <Typography paragraph={true}>
+        <Typography paragraph>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </Typography>
         <Link to='/contact' className={classes.link}>
