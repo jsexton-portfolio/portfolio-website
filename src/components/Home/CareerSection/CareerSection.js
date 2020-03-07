430ab63b67d442ab58313a5b1e459ecc4620d224
Justin Sexton <jsexton11295@gmail.com>
2020-03-07 17:18:59 -0600

    Fixed bug not allowing project cards to respond correctly

--- src/components/Home/CareerSection/CareerSection.js
+++ src/components/Home/CareerSection/CareerSection.js
@@ -1,5 +1,5 @@
-import { GridList, GridListTile, Typography } from '@material-ui/core'
-import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
+import { Container, Grid, Typography } from '@material-ui/core'
+import withWidth from '@material-ui/core/withWidth'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { ProjectCard } from '../ProjectCard/ProjectCard'
@@ -8,42 +8,26 @@ import { useStyles } from './style'
 const CareerSection = ({ width, projects }) => {
   const classes = useStyles()
 
-  const getGridListCols = () => {
-    if (isWidthUp('xl', width)) {
-      return 3
-    }
-
-    if (isWidthUp('lg', width)) {
-      return 2
-    }
-
-    if (isWidthUp('md', width)) {
-      return 2
-    }
-
-    return 1
-  }
-
   return (
-    <div>
+    <Container>
       <Typography variant='h3'>Career</Typography>
       <Typography paragraph>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </Typography>
-      <GridList cols={getGridListCols()} className={classes.projects} cellHeight={'auto'}>
+      <Grid container spacing={5} justify='center' className={classes.projects}>
         {projects.map(card => (
-          <GridListTile key={card.title} cols={1}>
+          <Grid key={card.title} item >
             <ProjectCard
               title={card.title}
               description={card.description}
               technologies={card.technologies}
               projectLink={card.projectLink}
             />
-          </GridListTile>
+          </Grid>
         ))}
-      </GridList>
-    </div>
+      </Grid>
+    </Container>
   )
 }
 
