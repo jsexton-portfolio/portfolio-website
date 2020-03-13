356c0437cb1e012327cc7e4e2092b53ee509f76d
Justin Sexton <jsexton11295@gmail.com>
2020-03-13 17:19:58 -0500

    Added contact suspending state page

---
+++ src/components/Contact/SuspendContact/SuspendContact.js
@@ -0,0 +1,12 @@
+import { Container, Typography } from '@material-ui/core'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import React from 'react'
+
+export const SuspendContact = () => {
+  return (
+    <Container style={{ textAlign: 'center', marginTop: 125, marginBottom: 125 }}>
+      <CircularProgress size={100} style={{ color: 'black' }}/>
+      <Typography paragraph style={{ marginTop: 20 }}>Submitting Your Contact Request</Typography>
+    </Container>
+  )
+}
