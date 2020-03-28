86ac6d1c7e9f1045022a199b909a8bca3c24cabe
Justin Sexton <jsexton11295@gmail.com>
2020-03-28 03:05:52 -0500

    Added technology section of career page

---
+++ src/components/Career/Technologies/TechnologyCardSection/TechnologyCardSection.js
@@ -0,0 +1,32 @@
+import { Grid, Typography } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export const TechnologyCardSection = ({
+  header,
+  cards,
+  ...other
+}) => {
+  const displayTechnologyCards = (cards) => {
+    return (
+      <Grid container justify='center' spacing={5}>
+        {cards.map((card, index) => (
+          <Grid key={index} item >
+            {card.source}
+          </Grid>
+        ))}
+      </Grid>)
+  }
+
+  return (
+    <div {...other}>
+      <Typography variant='h4'>{header}</Typography>
+
+      { displayTechnologyCards(cards) }
+    </div>)
+}
+
+TechnologyCardSection.propTypes = {
+  header: PropTypes.string.isRequired,
+  cards: PropTypes.array.isRequired
+}
---
+++ src/components/Career/Technologies/TechnologyCards/TechnologyCard.js
@@ -0,0 +1,49 @@
+import { Card, CardContent, CardHeader } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export const TechnologyCard = ({
+  name,
+  url,
+  children,
+  maxWidth = 150,
+  minWidth = 150,
+  elevate = true,
+  ...other
+}) => {
+  const [elevation, setElevation] = React.useState(5)
+
+  const handleMouseOver = () => elevate && setElevation(12)
+  const handleMouseOut = () => elevate && setElevation(5)
+
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ textDecoration: 'none' }}
+    >
+      <Card
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={{ maxWidth: maxWidth, minWidth: minWidth, marginTop: 25, marginBottom: 25, ...other }}
+        elevation={elevation}
+      >
+        <CardHeader title={name} style={{ textAlign: 'center' }}/>
+
+        <CardContent>
+          {children}
+        </CardContent>
+      </Card>
+    </a>
+  )
+}
+
+TechnologyCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  children: PropTypes.any,
+  maxWidth: PropTypes.number,
+  minWidth: PropTypes.number,
+  elevate: PropTypes.bool
+}
