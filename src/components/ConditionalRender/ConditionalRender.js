6a02847151f5487279c3a84f27cd775c82f6601a
Justin Sexton <jsexton11295@gmail.com>
2020-03-28 03:37:57 -0500

    Added conditional renderer component

---
+++ src/components/ConditionalRender/ConditionalRender.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+// Simple component used to lazily load render prop if condition is met.
+export const ConditionalRender = ({
+  condition,
+  render
+}) => {
+  return (
+    <React.Fragment>
+      {condition && render()}
+    </React.Fragment>
+  )
+}
+
+ConditionalRender.propTypes = {
+  condition: PropTypes.bool.isRequired,
+  render: PropTypes.func.isRequired
+}
