6c7525e125add5c9d54538dcbd55b0c7b01d1aa3
JSextonn <justinsexton.dev@gmail.com>
2020-06-29 23:38:48 -0500

    Added extent of content to page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,25 +22,31 @@ export const About = () => {
       <Container className={classes.container}>
         <Typography variant="h4">Passion for Programming</Typography>
         <Typography paragraph>
-          I started programming College introduced me to all the imporant ideas
-          I started programming College introduced me to all the imporant ideas
-          I started programming College introduced me to all the imporant ideas
+          My programming journey started about 10 years ago. I had enrolled in a
+          Computer Science course my freshman year of high school not knowing
+          exactly what it was. Needless to say at the time I thought I made a
+          huge mistake. I did not realize I had just made one of the best
+          decisions of my life. I completed the course, and sadly was not able
+          to appreciate it fully. It was not until that summer I started to
+          realize and appreciate what exactly was being accomplished in that
+          class. We were learning how to control computers. After coming to this
+          realization, I started teaching myself to build various games in
+          python using what little I had learned in my high school course. The
+          rest is history.
         </Typography>
+
         <Typography paragraph>
-          I then started teaching myself until I reached college I then started
-          teaching myself until I reached college I then started teaching myself
-          until I reached college I then started teaching myself until I reached
-          college I then started teaching myself until I reached college
+          I continued to program throughout high school polishing my skills by
+          surfing the web. Reading blogs and watching talks from professionals
+          at conventions were all great ways to ascend past the basics. Going
+          into college, I already had a very strong understanding of most core
+          programming constructs and had a few basic projects under my belt.
         </Typography>
+
         <Typography paragraph>
-          College introduced me to all the imporant ideas
-        </Typography>
-        <Typography paragraph>
-          I now work professionally as a programmer
-        </Typography>
-        <Typography paragraph>
-          In my spare time I still keep up with all the new stuff and try to
-          continue bettering myself
+          I now work professionally as a software developer. In my spare time I
+          still continue to explore exciting new technologies and better ways of
+          solving problems.
         </Typography>
       </Container>
     </Container>
