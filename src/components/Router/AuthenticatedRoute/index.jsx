f5f2d202af32f62924bb515bd170d685604743f9
Justin Sexton <justinsexton.dev@gmail.com>
2021-01-10 23:40:34 -0600

    Updated route guards to redirect to originally requested route after authenticating instead of always redirecting to dashboard root.

--- src/components/Router/AuthenticatedRoute/index.jsx
+++ src/components/Router/AuthenticatedRoute/index.jsx
@@ -1,13 +1,39 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { AuthenticatedRouteHelper } from '../AuthenticatedRouteHelper'
+import { useSelector } from 'react-redux'
+import { Redirect, Route } from 'react-router-dom'
 
-/**
- * Wrapper around the Route component. Only renders the route with the given component if the user is authenticated.
- * Otherwise, the page will be redirected to the given route, /login by default.
- */
-export const AuthenticatedRoute = ({ redirect = '/login', ...others }) => {
-  return <AuthenticatedRouteHelper redirect={redirect} {...others} />
+// The inverse of NonAuthenticatedRoute component.
+// Displays a given component only if the user is authenticated.
+// Otherwise, redirects to a chosen route. When redirecting, additional info is attached to the redirect
+// to help determine the exact route the user was originally trying to access.
+export const AuthenticatedRoute = ({
+  component: Component,
+  redirect = '/login',
+  ...others
+}) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+  const display = isAuthenticated
+  const { location } = others
+
+  return (
+    <Route
+      {...others}
+      location={location}
+      render={(props) =>
+        display ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: redirect,
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  )
 }
 
 AuthenticatedRoute.propTypes = {
--- src/components/Router/NonAuthenticatedRoute/index.jsx
+++ src/components/Router/NonAuthenticatedRoute/index.jsx
@@ -1,17 +1,37 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { AuthenticatedRouteHelper } from '../AuthenticatedRouteHelper'
+import { useSelector } from 'react-redux'
+import { Redirect, Route } from 'react-router-dom'
 
-/**
- * Inverse of the AuthenticatedRoute component. Only renders the component if the user is not authenticated.
- * If the user is authenticated, they are redirected to some other route.
- */
+// The inverse of AuthenticatedRoute component.
+// Displays a given component only if the user is not authenticated.
+// Otherwise, redirects to a chosen route.
 export const NonAuthenticatedRoute = ({
+  component: Component,
   redirect = '/dashboard',
   ...others
 }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+  const display = !isAuthenticated
+  const { location } = others
+  const pathname = location.state ? location.state.from.pathname : redirect
+
   return (
-    <AuthenticatedRouteHelper redirect={redirect} {...others} inverse={true} />
+    <Route
+      {...others}
+      location={location}
+      render={(props) =>
+        display ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: pathname
+            }}
+          />
+        )
+      }
+    />
   )
 }
 
