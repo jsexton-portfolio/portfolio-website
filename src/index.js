ea8b901183bc669be12a37e1a5069227a6b6f2d1
Justin Sexton <justinsexton.dev@gmail.com>
2021-03-10 01:35:36 -0600

    Added initial code for automatically updated expired bearer tokens

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -5,6 +6,7 @@ import { createStore } from 'redux'
 import { persistReducer, persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import storage from 'redux-persist/lib/storage'
+import authActions from './actions/auth'
 import App from './components/App'
 import './index.css'
 import rootReducer from './reducers'
@@ -20,6 +22,42 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = createStore(
   persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+axios.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    const originalRequest = error.config
+
+    console.log('Error response received')
+    console.log(`Has Retried: ${Boolean(originalRequest._retry)}`)
+    if (error.response.status === 401 && !originalRequest._retry) {
+      console.log('401 response received and trying to re-authenticate')
+      originalRequest._retry = true
+      const state = store.getState()
+      const refreshToken = state.auth.tokens.refreshToken
+      return axios
+        .post('https://api.justinsexton.net/security/refresh', {
+          refreshToken: refreshToken
+        })
+        .then((response) => {
+          const tokens = response.data.data
+          store.dispatch(
+            authActions.updateAuthInfo({
+              tokens: tokens
+            })
+          )
+          return axios(originalRequest)
+        })
+        .catch(() => {
+          store.dispatch(authActions.clearAuthInfo())
+        })
+    }
+
+    return Promise.reject(error)
+  }
 )
 
 const persistor = persistStore(store)
